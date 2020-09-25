800a32f6233a60017b72253bb2e4f1c85c1f6c04
Erikken-Hione <68046024+Erikken-Hione@users.noreply.github.com>
2020-09-25 10:09:09 +0600

    Updates

---
+++ precache-manifest.92d0daeea35788dcc4401df470664bfe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08e3949e18030eacb8ebaf68315ec14",
+    "url": "/RPG/index.html"
+  },
+  {
+    "revision": "1318b503c3de8f06d56c",
+    "url": "/RPG/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "e39d146bb0b3961d61c9",
+    "url": "/RPG/static/css/main.7c3e3f7f.chunk.css"
+  },
+  {
+    "revision": "1318b503c3de8f06d56c",
+    "url": "/RPG/static/js/2.b2926d63.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/RPG/static/js/2.b2926d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39d146bb0b3961d61c9",
+    "url": "/RPG/static/js/main.02524f05.chunk.js"
+  },
+  {
+    "revision": "a074495c28846bdcb295",
+    "url": "/RPG/static/js/runtime-main.858c60fd.js"
+  },
+  {
+    "revision": "29cbe0d4e755a74ea88bc92b0c597486",
+    "url": "/RPG/static/media/fall.29cbe0d4.png"
+  },
+  {
+    "revision": "f00a854a099ae572ef4f14a1e93dc830",
+    "url": "/RPG/static/media/flower.f00a854a.png"
+  },
+  {
+    "revision": "041bc7b2f0aa37db8daa000a0689b991",
+    "url": "/RPG/static/media/ninja.041bc7b2.png"
+  },
+  {
+    "revision": "4ad9d4a2daf95c131005d824aab1dfd6",
+    "url": "/RPG/static/media/spring.4ad9d4a2.png"
+  },
+  {
+    "revision": "a1cf1d290aa6eacaea887ccf47392b45",
+    "url": "/RPG/static/media/winter.a1cf1d29.png"
+  }
+]);
